d3c354d0ebb24cf02e9faa06fc36035ebd2ecd35
dovletov <kurban.dovletov@gmail.com>
2018-07-25 14:01:21 +0200

    Refactoring

--- utils/helper.py
+++ utils/helper.py
@@ -8,8 +8,11 @@ import tensorflow as tf
 import scipy.io
 
 
-
+# DATASETs
 def loadDatasetFromMat(dataset_name):
+    '''
+    Load image and corresponding label image from original mat files
+    '''
     if (dataset_name == "ss"):
         image = scipy.io.loadmat(os.path.join(ORIG_DATA_DIR, 'Salinas.mat'))['salinas']
         label = scipy.io.loadmat(os.path.join(ORIG_DATA_DIR, 'Salinas_gt.mat'))['salinas_gt']
@@ -54,11 +57,107 @@ def loadDatasetFromMat(dataset_name):
         image = scipy.io.loadmat(os.path.join(ORIG_DATA_DIR, 'Botswana.mat'))['Botswana']
         label = scipy.io.loadmat(os.path.join(ORIG_DATA_DIR, 'Botswana_gt.mat'))['Botswana_gt']
         print("---------------- dataset Botswana is loaded ----------------")
+    
     return image, label
 def getDatasetProperty(image, label):
+    '''
+    Get dataset properties such as height, width, depth of image and
+    class number of corresponding label image
+    '''
     if image.shape[0]==label.shape[0] and image.shape[1]==label.shape[1]:
-        height  = image.shape[0]
-        width   = image.shape[1]
-        depth   = image.shape[2]
+        height = image.shape[0]
+        width = image.shape[1]
+        depth = image.shape[2]
         class_number = label.max()
+    
     return height, width, depth, class_number
+
+
+# NORMALIZATIONs
+def reluNormalization(data):
+    '''
+    Normalize data between 0 and 1
+    '''
+    max_minus_min = data.max() - data.min()
+    data = data.astype(float)
+    data -= data.min()
+    data /= max_minus_min
+    
+    print("---------------- dataset normalized for ReLU ----------------")
+    print("min value =\t\t",data.min())
+    print("max value =\t\t",data.max())
+    
+    return data
+def tanhNormalization(data):
+    '''
+    Normalize data in range between -1 and 1
+    '''
+    max_minus_min = data.max() - data.min()
+    data = data.astype(float)
+    data -= data.min()
+    data /= max_minus_min
+    data *= 2
+    data -= 1
+    
+    print("---------------- dataset normalized for tanh ----------------")
+    print("min value =\t\t",data.min())
+    print("max value =\t\t",data.max())
+    
+    return data
+def meanNormalization(data):
+    '''
+    Mean Normalize (Standardize data)
+    '''
+    std = data.std()
+    data = data.astype(float)
+    data -= data.mean()
+    data /= std
+    
+    print("---------------- dataset mean normalized ----------------")
+    print("mean value =\t\t",data.mean())
+    print("std value =\t\t",data.std())
+    
+    return data
+
+
+# DATA PREPARATION
+def patchCentered(data, pos_x, pos_y, patch_size):
+    '''Patch input data of defined size centered at (pos_x, pos_y) 
+    coordinates and return it in ChHW (performance optimization)
+    '''
+    margin = (patch_size-1) // 2
+    
+    x_left = pos_x-margin
+    x_right = pos_x+margin+1
+    y_top = pos_y-margin
+    y_bottom = pos_y+margin+1
+
+    patch = data[x_left:x_right, y_top:y_bottom, :]
+    patch = np.transpose(patch,(2,0,1))
+    
+    return patch
+
+
+# TRAIN,VALID,EVAL coords
+def saveCoords(coords_file, tr_coords, vl_coords, ev_coords):
+    dictionary = {}
+    
+    dictionary["tr_coords"] = tr_coords
+    dictionary["vl_coords"] = vl_coords
+    dictionary["ev_coords"] = ev_coords
+    
+    scipy.io.savemat(coords_file, dictionary)
+    print(coords_file)
+    print("---------------- TRAIN/VALID/TEST COORDS are saved   ----------------")
+def loadCoords(coords_file):
+    coords = scipy.io.loadmat(coords_file)
+    
+    tr_coords = coords['tr_coords'][0]
+    vl_coords = coords['vl_coords'][0]
+    ev_coords = coords['ev_coords'][0]    
+    
+    print("---------------- TRAIN/VALID/TEST COORDS are loaded ----------------")
+    print("lenght tr coords\t",len(tr_coords))
+    print("length vl coords\t",len(vl_coords))
+    print("length ev coords\t",len(ev_coords))
+    return tr_coords, vl_coords, ev_coords
