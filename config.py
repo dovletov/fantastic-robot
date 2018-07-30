1c0ef5e192560502c10bb21112002a98ccfffc50
dovletov <kurban.dovletov@gmail.com>
2018-07-30 15:18:30 +0200

    Refactoring

--- config.py
+++ config.py
@@ -1,9 +1,11 @@
 import os
 
+# change if needed
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-ORIG_DATA_DIR = os.path.join(BASE_DIR, 'original_data') # change if needed
-RESULTS_DIR = '/tensorflow/sjgudovl/results' # change if needed
+ORIG_DATA_DIR = os.path.join(BASE_DIR, 'original_data')
+RESULTS_DIR = '/tensorflow/sjgudovl/results'
 
+# constants
 COLORS_D17  = ['white', '#e6194b', '#3cb44b', '#ffe119', '#0082c8', \
                         '#f58231', '#911eb4', '#46f0f0', '#f032e6', \
                         '#d2f53c', '#fabebe', '#008080', '#e6beff', \
